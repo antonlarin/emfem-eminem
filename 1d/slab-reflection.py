3e97b374851aea1368da63ff2770ba02c1efa299
Anton Larin <edding.default@gmail.com>
2016-11-13 16:59:20 +0300

    Adapt slab reflection problem to order generalization

--- 1d/slab-reflection.py
+++ 1d/slab-reflection.py
@@ -6,10 +6,10 @@ import math, cmath
 import numpy as np
 import matplotlib.pyplot as plt
 
-from fem_1d import BoundaryCondition, fem_1d, LINEAR
+from fem_1d import BoundaryCondition, fem_1d, LINEAR, QUADRATIC, CUBIC
 
 
-def compute_reflection_ez(theta, element_count):
+def compute_reflection_ez(theta, order, element_count):
     # physical problem setup
     L = 1
     mu_r = lambda x: 2 - .1j
@@ -19,7 +19,7 @@ def compute_reflection_ez(theta, element_count):
 
     # FEM setup
     l = L / element_count
-    mesh = np.linspace(0, L, element_count + 1)
+    mesh = np.linspace(0, L, order * element_count + 1)
     alpha = lambda x: 1 / mu_r(x)
     beta = lambda x: -k0**2 * (eps_r(x) - 1 / mu_r(x) * math.sin(theta)**2)
     f = lambda x: 0
@@ -34,20 +34,20 @@ def compute_reflection_ez(theta, element_count):
     )
 
     E_zs = fem_1d(
+            order,
             mesh,
             alpha,
             beta,
             f,
             left_bc,
             right_bc,
-            LINEAR,
             dtype='complex')
 
-    return ((E_zs[-1] - E0 * cmath.exp(1j * k0 * L * math.cos(theta))) /
+    return ((E_zs.phis[-1] - E0 * cmath.exp(1j * k0 * L * math.cos(theta))) /
         (E0 * cmath.exp(-1j * k0 * L * math.cos(theta))))
 
 
-def compute_reflection_hz(theta, element_count):
+def compute_reflection_hz(theta, order, element_count):
     # physical problem setup
     L = 1
     mu_r = lambda x: 2 - .1j
@@ -57,7 +57,7 @@ def compute_reflection_hz(theta, element_count):
 
     # FEM setup
     l = L / element_count
-    mesh = np.linspace(0, L, element_count + 1)
+    mesh = np.linspace(0, L, order * element_count + 1)
     alpha = lambda x: 1 / eps_r(x)
     beta = lambda x: -k0**2 * (mu_r(x) - 1 / eps_r(x) * math.sin(theta)**2)
     f = lambda x: 0
@@ -72,33 +72,36 @@ def compute_reflection_hz(theta, element_count):
     )
 
     H_zs = fem_1d(
+            order,
             mesh,
             alpha,
             beta,
             f,
             left_bc,
             right_bc,
-            LINEAR,
             dtype='complex')
 
-    return ((H_zs[-1] - H0 * cmath.exp(1j * k0 * L * math.cos(theta))) /
+    return ((H_zs.phis[-1] - H0 * cmath.exp(1j * k0 * L * math.cos(theta))) /
         (H0 * cmath.exp(-1j * k0 * L * math.cos(theta))))
 
 
 def main():
     runs = 30
     thetas = np.linspace(0, math.pi / 2, runs)
-    rs_50 = np.zeros(runs)
-    rs_100 = np.zeros(runs)
+    rs_linear = np.zeros(runs)
+    rs_quadratic = np.zeros(runs)
+    rs_cubic = np.zeros(runs)
     for i, theta in enumerate(thetas):
-        rs_50[i] = abs(compute_reflection_ez(theta, 50))**2
-        rs_100[i] = abs(compute_reflection_ez(theta, 100))**2
+        rs_linear[i] = abs(compute_reflection_ez(theta, LINEAR, 50))**2
+        rs_quadratic[i] = abs(compute_reflection_ez(theta, QUADRATIC, 50))**2
+        rs_cubic[i] = abs(compute_reflection_ez(theta, CUBIC, 50))**2
 
     degrees = map(lambda theta: 180 * theta / math.pi, thetas)
 
     # plot results
-    plt.plot(degrees, rs_50, 'b', label='FEM 50 elems')
-    plt.plot(degrees, rs_100, 'r', label='FEM 100 elems')
+    plt.plot(degrees, rs_linear, 'b', label='FEM linear')
+    plt.plot(degrees, rs_quadratic, 'r', label='FEM quadratic')
+    plt.plot(degrees, rs_cubic, 'g', label='FEM cubic')
 
     plt.grid()
     plt.legend(loc='best')
@@ -109,12 +112,14 @@ def main():
 
     plt.clf()
     for i, theta in enumerate(thetas):
-        rs_50[i] = abs(compute_reflection_hz(theta, 50))**2
-        rs_100[i] = abs(compute_reflection_hz(theta, 100))**2
+        rs_linear[i] = abs(compute_reflection_hz(theta, LINEAR, 50))**2
+        rs_quadratic[i] = abs(compute_reflection_hz(theta, QUADRATIC, 50))**2
+        rs_cubic[i] = abs(compute_reflection_hz(theta, CUBIC, 50))**2
 
     # plot results
-    plt.plot(degrees, rs_50, 'b', label='FEM 50 elems')
-    plt.plot(degrees, rs_100, 'r', label='FEM 100 elems')
+    plt.plot(degrees, rs_linear, 'b', label='FEM linear')
+    plt.plot(degrees, rs_quadratic, 'r', label='FEM quadratic')
+    plt.plot(degrees, rs_cubic, 'g', label='FEM cubic')
 
     plt.grid()
     plt.legend(loc='best')
